c55119a808f6547cd3b86f014a67b66c198a0cf0
jaydennguyen2000 <nguyenp26@student.douglascollege.ca>
2020-03-31 20:21:51 -0700

    Adding the add button to the action bar

--- app/src/main/java/com/example/sn34ker/MainActivity.java
+++ app/src/main/java/com/example/sn34ker/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.sn34ker;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,6 +24,19 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.actionbar_item, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Toast.makeText(this, "Selected", Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
